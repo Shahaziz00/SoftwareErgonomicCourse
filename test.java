fad72179f2be0b60b346dc25fcea40d4d76fe858
tobim1996 <tobim1996@outlook.de>
2021-10-05 06:45:26 +0200

    test
    test

--- test.java
+++ test.java
@@ -4,5 +4,5 @@ public class test
        public static void main (String[] args)
        {
              // Ausgabe Hello World!
-             System.out.println("Hello World!");
+             System.out.println("Hello World! and test");
        }
